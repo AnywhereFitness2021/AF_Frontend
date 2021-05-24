7e62efa553a4474c453a529812de0a1aebfd18ad
Sergio Alvarez <salvare736@gmail.com>
2021-05-24 00:11:09 -0400

    installed additional dependencies - setup basic styling and app structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//TECH IMPORTS
+import React from 'react';
+//STYLING IMPORTS
+import './index.css';
+import Logo from './AnywhereFitnessLogo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="logoAndHeading"></div>
+      <img className="logo" src={Logo} alt="gym barbell"/>
+      <h1 className="mainHeading">Anywhere Fitness</h1>
+    </>
   );
 }
 
