cd160b4164473e04896069472e748699f8119c55
Michael-Kochis <rhishisikk@gmail.com>
2021-05-24 12:55:24 -0500

    yup schema and error messages appear to work

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import '../styles/login.css'
+import * as yup from 'yup'
+import { schema } from '../schema/loginSchema'
 
 function LoginForm(props) {
     const initialValues = {
@@ -7,22 +9,41 @@ function LoginForm(props) {
         password: "",
     }
     const [form, setForm] = useState(initialValues);
+    const [shaped, setShaped] = useState({});
+
+    const checkSchema = (name, value) => {
+        yup.reach(schema, name).validate(value)
+            .then(() => {
+                setShaped({...shaped, [name]: ''});
+            }).catch((err) => {
+                if (err.errors) { 
+                    setShaped({...shaped, [name]: err.errors[0]});
+                }
+        });
+    }
 
     const handleChange = (event) => {
         const { name, type, value, checked } = event.target;
         const updateData = (type === 'checkbox')?checked:value;
         setForm({...form, [name]: updateData});
+        checkSchema(name, updateData);
     }
 
     return (
         <div id="form-login" className="form form-login">
             <h1>Login Form</h1>
+            <div id="error-login" className="error error-login" >
+                {shaped.username}</div>
+            <div id="error-username" className="error error-username" ></div>
             <label id="label-username" htmlFor="username">Username
                 <input id="username" name="username" type="text" 
                     onChange={handleChange} value={form.username} />
             </label>
+            <div id="error-password" className="error error-password" >
+                {shaped.password}
+            </div>
             <label id="label-password" htmlFor="password">Password
-                <input id="password" name="password" type="text"
+                <input id="password" name="password" type="password"
                 onChange={handleChange} value={form.password} />
             </label>
             {props.children}
--- src/components/register.js
+++ src/components/register.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import '../styles/login.css'
+import * as yup from 'yup'
+import { schema } from '../schema/loginSchema'
 
 function RegisterForm(props) {
     const initialValues = {
@@ -9,29 +11,47 @@ function RegisterForm(props) {
         role: ""
     }
     const [form, setForm] = useState(initialValues);
+    const [shaped, setShaped] = useState({});
+
+    const checkSchema = (name, value) => {
+        yup.reach(schema, name).validate(value)
+            .then(() => {
+                setShaped({...shaped, [name]: ''});
+            }).catch((err) => {
+                if (err.errors) { 
+                    setShaped({...shaped, [name]: err.errors[0]});
+                }
+        });
+    }
 
     const handleChange = (event) => {
         const { name, type, value, checked } = event.target;
         const updateData = (type === 'checkbox')?checked:value;
         setForm({...form, [name]: updateData});
-        console.log(form);
+        checkSchema(name, updateData);
     }
 
     return (
         <div id="form-register" className="form form-register">
             <h1>Register Form</h1>
+            <div id="error-register" className="error error-register" ></div>
+            <div id="error-username" className="error error-username" >{shaped.username}</div>
             <label id="label-username" htmlFor="username">Username
                 <input id="username" name="username" type="text" 
                     onChange={handleChange} value={form.username}/>
             </label>
+            <div id="error-password" className="error error-password" >
+                {shaped.password}</div>
             <label id="label-password" htmlFor="password">Password
                 <input id="password" name="password" type="password"
                     onChange={handleChange} value={form.password} />
             </label>
+            <div id="error-confirm-password" className="error error-confirm-password" >{shaped.confirmPassword}</div>
             <label id="label-confirm-password" htmlFor="confirmPassword">Confirm Password
                 <input id="confirmPassword" name="confirmPassword" type="password" onChange={handleChange} 
                 value={form.confirmPassword} />
             </label>
+            <div id="error-role" className="error error-role" >{shaped.role}</div>
             <label id="label-role" htmlFor="role" onChange={handleChange} 
                 value={form.role} >Role
                 <select id="role" name="role">
